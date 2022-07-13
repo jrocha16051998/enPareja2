b31d1d1517b6616326eef3cd7a62918407aa2603
Andres Rocha <juanrocha.exe@gmail.com>
2022-07-13 21:04:02 +0100

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
     return (
         <nav className="navbar shadow header mb-1">
             <div className="container-fluid justify-content-center">
-                <a className="navbar-brand" href="/"><h4 className='display-6'>💏  EnPareja2  💏</h4></a>
+                <a className="navbar-brand" href="https://github.com/jrocha16051998/enPareja2/"><h4 className='display-6'>💏  EnPareja2  💏</h4></a>
                 <hr />
             </div>
         </nav>
